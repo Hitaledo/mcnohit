ff4d5237c2b0e870e6820051729c4e165501a5be
Hatzoru <119678571+Hatzoru@users.noreply.github.com>
2022-12-02 11:10:10 -0300

    Update Onhit.java

--- src/main/java/com/hitaledo/mcnohit/Onhit.java
+++ src/main/java/com/hitaledo/mcnohit/Onhit.java
@@ -7,7 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 //Clase publica Onhit implementa un Listener
 public class Onhit implements Listener{
-    float tHit=0.1;
+    double tHit=0.1;
     //Funcion onhit con el evento EntityDamage
     @EventHandler
     public void onhit(EntityDamageEvent event){
